cd9e2c03f3f378ccecf4f1487c3dfda77a39f978
SudipBaidyadev <139113892+SudipBaidyadev@users.noreply.github.com>
2023-12-09 08:43:55 +0530

    origin

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,6 @@ router.post('/signin', signin);
 // /api/logout
 router.get('/logout', logout);
 // /api/me
-router.get('/me', isAuthenticated, userProfile);
+router.get('/me', userProfile);
 
 module.exports = router;
