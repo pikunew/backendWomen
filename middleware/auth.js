d83388f9b0d878e616703689a1062b0054cb6f9e
SudipBaidyadev <139113892+SudipBaidyadev@users.noreply.github.com>
2023-12-09 07:45:13 +0530

    origin

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,29 +2,33 @@ const ErrorResponse = require('../utils/errorResponse');
 const jwt = require('jsonwebtoken');
 const User = require("../models/userModel");
 
-// check is user is authenticated
+// Check if the user is authenticated
 exports.isAuthenticated = async (req, res, next) => {
     const { token } = req.cookies;
+
     // Make sure token exists
     if (!token) {
-        return next(new ErrorResponse('Not authorized to access this route', 401));
+        return next(new ErrorResponse('Not authorized to access this route - Token not found', 401));
     }
 
     try {
         // Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id);
+        console.log('Authentication successful!');
         next();
 
     } catch (error) {
-        return next(new ErrorResponse('Not authorized to access this route', 401));
+        console.error('Authentication error:', error);
+        return next(new ErrorResponse('Not authorized to access this route - Token verification failed', 401));
     }
 }
 
-//middleware for admin
+// Middleware for admin
 exports.isAdmin = (req, res, next) => {
     if (req.user.role === 0) {
-        return next(new ErrorResponse('Access denied, you must an admin', 401));
+        return next(new ErrorResponse('Access denied - You must be an admin', 401));
     }
+    console.log('Admin access granted!');
     next();
 }
